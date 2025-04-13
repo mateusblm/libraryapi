7f9bf23b92e9ad3f246ffd7333f4d95bf1903717
mateusblm <mateusblm@outlook.com>
2025-04-13 20:13:22 -0300

    feat: Criando e mapeando entidade Autor

---
+++ src/main/java/io/github/mateusbm/libraryapi/model/Autor.java
@@ -0,0 +1,32 @@
+package io.github.mateusbm.libraryapi.model;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name="autor", schema = "public")
+@Getter
+@Setter
+public class Autor {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID) // Id gerado automaticamente
+    private UUID id;
+
+    @Column(name = "nome", length = 100, nullable = false) // Varchar(100), not null
+    private String nome;
+
+    @Column(name = "data_nascimento", nullable = false)
+    private LocalDate dataNascimento;
+
+    @Column(name = "nacionalidade", length = 50, nullable = false)
+    private String nacionalidade;
+
+
+}
