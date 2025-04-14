6a780abfa6d8746f086b8ccd62219df128c07bac
Mateus Burlamaqui <mateusblm@outlook.com>
2025-04-14 18:10:54 -0300

    test: Testando funcionalidade de salvar registro.

--- src/main/java/io/github/mateusbm/libraryapi/Application.java
+++ src/main/java/io/github/mateusbm/libraryapi/Application.java
@@ -1,13 +1,30 @@
 package io.github.mateusbm.libraryapi;
 
+import io.github.mateusbm.libraryapi.model.Autor;
+import io.github.mateusbm.libraryapi.repository.AutorRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+
+		var context = SpringApplication.run(Application.class, args);
+		AutorRepository repository = context.getBean(AutorRepository.class);
+		exemploSalvarRegistro(repository);
+	}
+
+	public static void exemploSalvarRegistro(AutorRepository autorRepository) {
+		Autor autor = new Autor();
+		autor.setNome("Mateus");
+		autor.setNacionalidade("Brasileira");
+		autor.setDataNascimento(LocalDate.of(2005, 4, 8));
+
+		var autorSalvo = autorRepository.save(autor);
+		System.out.println("Autor salvo: " + autorSalvo);
 	}
 
 }
